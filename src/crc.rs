e62b6869e3b59f08e0cf76656258a6a43c4be38b
Aaron Cunningham <aa.cunningham@outlook.com>
2023-05-29 20:31:02 -0700

    Remove once_cell, use const

--- src/crc.rs
+++ src/crc.rs
@@ -1,20 +1,22 @@
-use once_cell::sync::Lazy;
-
-static CRC_TABLE: Lazy<Vec<u32>> = Lazy::new(|| {
-    let mut table = Vec::with_capacity(u8::MAX as usize + 1);
-    for n in u8::MIN..=u8::MAX {
+const CRC_TABLE: [u32; 256] = {
+    let mut table = [0; 256];
+    let mut n = 0;
+    while n < 256 {
         let mut c = n as u32;
-        for _ in 0..8 {
+        let mut i = 0;
+        while i < 8 {
             if c & 1 != 0 {
                 c = 0xedb88320 ^ (c >> 1);
             } else {
                 c >>= 1;
             }
+            i += 1;
         }
-        table.push(c);
+        table[n as usize] = c;
+        n += 1;
     }
     table
-});
+};
 
 fn update_crc<I: IntoIterator<Item = u8>>(crc: u32, data: I) -> u32 {
     let mut new_crc = crc;
